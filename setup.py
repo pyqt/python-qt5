15984b07b86c4f0e46840f5dc43b332634bc65ac
Marcus Ottosson <konstruktion@gmail.com>
2014-09-29 12:14:15 +0100

    Distribute Windows installer and bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
 import os
+import sys
+
 from setuptools import setup, find_packages
+
+
+def get_version():
+    sys.path.insert(os.path.dirname(__file__))
+    import PyQt5
+    return PyQt5.__version__
+
+
+def get_package_data():
+    package_data = dict()
+
+    package_data['PyQt5'] = list()
+    for subdir in ("plugins/", "qml/", "uic/"):
+        abspath = os.path.abspath("PyQt5/" + subdir)
+        for root, dirs, files in os.walk(abspath):
+            for f in files:
+                fpath = os.path.join(root, f)
+                relpath = os.path.relpath(fpath, abspath)
+                relpath = relpath.replace("\\", "/")
+                package_data['PyQt5'].append(subdir + relpath)
+
+    package_data['PyQt5'].extend(["*.exe", "*.dll", "*.pyd"])
+    return package_data
+
 
 classifiers = [
     'Intended Audience :: Developers',
@@ -12,23 +38,9 @@ classifiers = [
 ]
 
 
-package_data = dict()
-
-package_data['PyQt5'] = list()
-for subdir in ("plugins/", "qml/", "uic/"):
-    abspath = os.path.abspath("PyQt5/" + subdir)
-    for root, dirs, files in os.walk(abspath):
-        for f in files:
-            fpath = os.path.join(root, f)
-            relpath = os.path.relpath(fpath, abspath)
-            relpath = relpath.replace("\\", "/")
-            package_data['PyQt5'].append(subdir + relpath)
-
-package_data['PyQt5'].extend(["*.exe", "*.dll", "*.pyd"])
-
 setup(
     name='pypiqt5',  # To avoid name-clash with existing PyQt5
-    version="0.1.1",
+    version=get_version(),
     description='PyQt5',
     long_description="pypiqt5",
     author='Marcus Ottosson',
@@ -38,5 +50,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     classifiers=classifiers,
-    package_data=package_data
+    package_data=get_package_data()
 )
